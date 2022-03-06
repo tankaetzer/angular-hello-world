dfbb9e52ba00e8f585ec3b37954bb6c13166785b
Kae Tzer <kaetzer.tan@ifastfinancial.com>
2022-03-06 12:46:37 +0800

    Displaying Data and Handling Events: custom pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CourseComponent } from './course/course.component';
+import { SummaryPipe } from './summary.pipe';
 
 @NgModule({
-  declarations: [AppComponent, CourseComponent],
+  declarations: [AppComponent, CourseComponent, SummaryPipe],
   imports: [FormsModule, BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -12,6 +12,8 @@ export class CourseComponent implements OnInit {
   courses: Array<string> = [];
   imageUrl: string = 'https://picsum.photos/250?image=9';
   email: string = 'tankaetzer@gmail.com';
+  text: string =
+    'As they rounded a bend in the path that ran beside the river, Lara recognized the silhouette of a fig tree atop a nearby hill. The weather was hot and the days were long. The fig tree was in full leaf, but not yet bearing fruit. Soon Lara spotted other landmarks—an outcropping of limestone beside the path that had a silhouette like a man’s face, a marshy spot beside the river where the waterfowl were easily startled, a tall tree that looked like a man with his arms upraised. They were drawing near to the place where there was an island in the river. The island was a good spot to make camp. They would sleep on the island tonight.';
 
   course = {
     title: 'The Complete Angular Course',
